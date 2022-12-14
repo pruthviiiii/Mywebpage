311df8a438b0f2ab183cb3f98f24af2a2bea0a0a
mant3sh <mahantesha.bv1999@gmail.com>
2022-12-14 10:59:30 +0530

    white balance

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,12 +8,15 @@ module.exports = {
         'light-head':'#808080',
         'Dark-modeC':'#1a202c',
         'Dark-text':'#37352f',
+        // 'Dark-text':'#040404',
         'light-text':'#e2e8f0',
-        'light-modeC':'#f7fafc',
-        'nav-light':'rgba(247, 250, 252, 0.8);',
+        'light-modeC':'#fffefc',
+        'nav-light':'rgba(255, 254, 252, 0.8);',
         'nav-dark':'rgba(26, 32, 44, 0.8);'
       }
     },
   },
   plugins: [],
 }
+// fffefc
+// f7fafc
